8bbc9f8263a7fbd00c968f1cc9a2277858760037
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-02-16 21:34:27 -0600

    added a few routers and tested capabilities of bootstrap, must discuss with team further before using this because it's confusing asf lmfao

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import { createBrowserRouter, RouterProvider, Route, Outlet } from 'react-router-dom';
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Navibar from "./components/Navbar.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./scss/styles.scss";
+const Layout = () => {
+    return (
+        <>
+        <Navibar />
+        <Outlet />
+        <Footer />
+        </>
+    );
+};
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+        ]
+    },
+    {
+        path: "/register",
+        element: <Register/>
+    },
+    {
+        path: "/login",
+        element: <Login/>
+    },
+]);
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="app">
+            <div className="container">
+                <RouterProvider router={router} />
+                
+            </div>
+        </div>
+    );
 }
 
 export default App
---
+++ front-end/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+const Navibar = () => {
+    return (
+       <>
+       <h1>Navbar</h1>
+       </>
+    );
+};
+export default Navibar
