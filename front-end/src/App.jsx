8220c2ecfe8913991c88b2f1499d20f151a35ede
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-02-22 13:37:16 -0600

    changes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -46,12 +46,11 @@ const router = createBrowserRouter([
 ]);
 function App() {
     return (
-        <div className="app">
-            <div className="container">
+        
+            
                 <RouterProvider router={router} />
                 
-            </div>
-        </div>
+           
     );
 }
 
