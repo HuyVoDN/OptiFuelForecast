e575a7beae21aec3947cfb5dda15413fe96eb4b1
ashnapatel23 <124214858+ashnapatel23@users.noreply.github.com>
2024-02-21 23:01:33 -0600

    client profile 1st file

---
+++ front-end/src/pages/ClientProfile.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class ClientProfile extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>My Profile</h1>
+                <form id="client-profile-form">
+                    <div className="form-group">
+                        <label htmlFor="first-name">First Name:</label>
+                        <input type="text" id="first-name" name="first-name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="last-name">Last Name:</label>
+                        <input type="text" id="last-name" name="last-name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="address">Address:</label>
+                        <input type="text" id="address" name="address" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City:</label>
+                        <input type="text" id="city" name="city" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State:</label>
+                        <input type="text" id="state" name="state" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="zipcode">Zipcode:</label>
+                        <input type="text" id="zipcode" name="zipcode" required />
+                    </div>
+                    <div className="button">
+                        <button>Save</button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ClientProfile;
