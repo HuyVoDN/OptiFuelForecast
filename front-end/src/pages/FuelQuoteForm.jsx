f56bfaac3ac6aac533ed4c12a5031def7113c07a
Thinh Ngo <ngothinh2511@gmail.com>
2024-02-23 12:17:10 -0600

    -updating fuelquoteform

--- front-end/src/pages/FuelQuoteForm.jsx
+++ front-end/src/pages/FuelQuoteForm.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./FuelQuoteForm.scss";
 import { Link } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar.jsx";
-import NavBar from "../components/Navbar.jsx";
+import NavBar from "../components/navbar/Navbar.jsx";
 
 const Quote = () => {
     return (
         <>
             <NavBar/>
             <Sidebar />
+            {/* fuel quote form here,  */}
         </>
     );
 }
