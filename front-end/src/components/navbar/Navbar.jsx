e97714f1a6dd746d5ad2c91555de9fc24412e9e1
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2024-02-23 11:00:07 -0600

    navbar being acoustic so i changed the src

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
        <>
        <div className="mainnavbar">
-            <Link to='/'><img className="optiLogo" src="src/assets/optifuelforecastlogo.png" alt="logo" /></Link>
+            <Link to='/'><img className="optiLogo" src={Logo} alt="logo" /></Link>
             <div className="mainnavbar-right">
                 <nav className="mainnavbar-links">
                     <NavLink className="link" to='/'>Home</NavLink>
