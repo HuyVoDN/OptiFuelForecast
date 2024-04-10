d54a543de9c06a159acc0fd0f39db03a20e43366
Suchanut Namcharoen <suchanut3134@gmail.com>
2024-04-09 20:44:51 -0500

    about page styling

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import "./Home.scss";
 import { Link } from "react-router-dom";
+import About from '../pages/about/About';
 
 const Home = () => {
     return (
         <>
-            <div className="home">
-                <div className="desc">
-                    <h1>We explore to empower</h1>
-                    <p>
-                        Our cutting-edge oil fuel forecast system utilizes
-                        advanced analytics and real-time data to <br />
-                        provide accurate predictions and insights
-                        into the future of oil prices and consumption. <br />
-                        With a focus on innovation and empowerment,
-                        we strive to contribute <br />to a sustainable and efficient
-                        energy landscape.
-                    </p>
-                    <Link className="link" to="/about">LEARN MORE</Link>
-                </div>
+        <div className="home">
+            <div className="desc">
+                <h1>We explore to empower</h1>
+                <p>
+                    Our cutting-edge oil fuel forecast system utilizes
+                    advanced analytics and real-time data to <br />
+                    provide accurate predictions and insights
+                    into the future of oil prices and consumption. <br />
+                    With a focus on innovation and empowerment,
+                    we strive to contribute <br />to a sustainable and efficient
+                    energy landscape.
+                </p>
+                <Link className="link" to="/about">LEARN MORE</Link>
             </div>
-            </>
-        
+            <div className="curve"></div>
+        </div>
+        <div className="about-container">
+            <About/>
+        </div>
+        </>
+
     );
 }
 export default Home
--- front-end/src/pages/about/About.jsx
+++ front-end/src/pages/about/About.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./About.scss";
-import Carousel from '../components/carousel/carousel';
+import Carousel from '../../components/carousel/carousel';
 
 const About = () => {
   return (
     <div className='about-page-container'>
-      <div className="about-page-header">
+      {/* <div className="about-page-header">
         <h2>Opti Fuel Forecast</h2>
         <span>Fueling Your Future with Precision and Care!</span>
-      </div>
+      </div> */}
         <Carousel/>
         <p>Describe the Fuel Form purposes</p>
     </div>
