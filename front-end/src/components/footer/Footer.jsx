a9dae184ac0c184dc37f1e61d840ce11909d2d41
ashnapatel23 <124214858+ashnapatel23@users.noreply.github.com>
2024-04-10 22:53:48 -0500

    adding more footer stuff

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -4,9 +4,24 @@ import "./Footer.scss";
 const Footer = () => {
     return (
         <footer className="footer">
+
+        <div className="logo">
+            <img src="" alt="Logo"/>
+        </div>
+
+        <div className="footer_links">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+        </div>
+
         <div className="footer-content">
             <p>OptiFuelForecast</p>
         </div>
+
+        <div className="copyright">
+            <p>&copy; 2024 OptiFuelForecast. All rights reserved.</p>
+        </div>
+
         </footer>
     );
 };
