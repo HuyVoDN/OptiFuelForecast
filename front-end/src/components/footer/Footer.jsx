0cf38f2b2e9e20cf545feaa2de1c05c806983666
ashnapatel23 <124214858+ashnapatel23@users.noreply.github.com>
2024-04-11 20:05:55 -0500

    Adding logo

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Footer.scss";
+import Logo from "../../assets/optifuelforecastlogo.png";
 
 const Footer = () => {
     return (
         <footer className="footer">
 
         <div className="logo">
-            <img src="" alt="Logo"/>
+            <Link to='/'><img className="optiLogo" src={Logo} alt="logo" /></Link>
         </div>
 
         <div className="footer_links">
