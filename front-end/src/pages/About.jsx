237e3ce675a90d057f40def012e2c10a5c962532
Thinh Ngo <ngothinh2511@gmail.com>
2024-04-12 22:04:48 -0500

    added favicon and fixed About page

--- front-end/src/pages/About.jsx
+++ front-end/src/pages/About.jsx
@@ -3,22 +3,25 @@ import "./About.scss";
 
 const About = () => {
   return (
-    <about className="footer">
+    // <about className="footer">
+    <>
+      <div className="about_wrapper">
 
-    <div className="header">
-        <h2>About</h2>
-    </div>
+        <div className="content_about">
+          <h1>About</h1>
+          <p>
+            At OptiFuelForecast, we are excited to start on this project, aiming to develop a prediction based software application. <br/>
+            The goal is to predict fuel rates with precision and efficiency. 
+            Our solution will revolutionize how fuel rates are determined.<br/> 
+            Using knowledge of our client like their location, historical data, requested gallons, and company profit margin we aim to give <br/>the most accurate predication. 
+            By using data analytics, we're committed to given an online platform that gives businesses a chance <br/>to make informed decisions in real-time.
+            With our expertise and dedication to innovation, we look forward to crafting a solution.
+          </p>
+        </div>
+      </div>
 
-    <div className="content_about">
-      <p>At OptiFuelForecast, we are excited to start on this project, aiming to develop a prediction based software application. 
-        The goal is to predict fuel rates with precision and efficiency. 
-        Our solution will revolutionize how fuel rates are determined. 
-        Using knowledge of our client like their location, historical data, requested gallons, and company profit margin we aim to give the most accurate predication. 
-        By using data analytics, we're committed to given an online platform that gives businesses a chance to make informed decisions in real-time.
-        With our expertise and dedication to innovation, we look forward to crafting a solution.</p>
-    </div>
-
-    </about>
+      {/* </about> */}
+    </>
   );
 };
 
