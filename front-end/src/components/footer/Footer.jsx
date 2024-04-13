478b69148e1d20484b7f253af522ae95dfba414a
Thinh Ngo <ngothinh2511@gmail.com>
2024-04-12 20:32:15 -0500

    make the footer not so haram anymore

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Logo from "../../assets/OptiFuelForecast_removed_background.png";
 import "./Footer.scss";
 
 const Footer = () => {
+    const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+    useEffect(() => {
+        // Update current year when component mounts
+        setCurrentYear(new Date().getFullYear());
+    }, []);
+
     return (
         <footer className="footer">
 
-        <div className="logo">
-            <img src="" alt="Logo"/>
-        </div>
+            {/*<div className="footer_links">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+            </div>*/}
 
-        <div className="footer_links">
-            <a href="#">Home</a>
-            <a href="#">About</a>
-        </div>
+            {/* <div className="footer-content">
+                <p>OptiFuelForecast</p>
+            </div> */}
 
-        <div className="footer-content">
-            <p>OptiFuelForecast</p>
-        </div>
+            <div className="copyright">
+                <p> Copyright &copy; <span>{currentYear}</span> OptiFuelForecast. All rights reserved.</p>
+            </div>
 
-        <div className="copyright">
-            <p>&copy; 2024 OptiFuelForecast. All rights reserved.</p>
-        </div>
-
+            {/* <div className="logo">
+                <img src={Logo} alt="Logo"/>
+            </div>  */}
         </footer>
     );
 };
